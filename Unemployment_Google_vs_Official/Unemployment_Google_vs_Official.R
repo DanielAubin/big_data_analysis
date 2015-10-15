dd0cc2dacc7f9a6a8b459b1c9cc8b47f64ef39b6
DanielAubin <DanielAubin@users.noreply.github.com>
2015-10-15 10:33:49 +0200

    Update Unemployment_Google_vs_Official.R

--- Unemployment_Google_vs_Official/Unemployment_Google_vs_Official.R
+++ Unemployment_Google_vs_Official/Unemployment_Google_vs_Official.R
@@ -1,4 +1,33 @@
+### HEADER ###
+# ------------------------------------------------------  
+# Name of QuantLet:  Stacked Barplot
+# ------------------------------------------------------  
+# Published in:  Not Published 
+# ------------------------------------------------------  
+# Description:  Creates two stacked bar-plots with legend sorted by amount and plots them all together.
+# ------------------------------------------------------  
+# Keywords:  
+# ------------------------------------------------------  
+# See also:  
+# ------------------------------------------------------  
+# Author:  New
+# ------------------------------------------------------  
+# Submitted:  Daniel Jacob, 2015/10/15
+# ------------------------------------------------------  
+# Datafile:  GoogleTrends Dataset (search term: "Arbeitsamt"), Unemployment-rate from Statistisches Bundesamt
+# ------------------------------------------------------  
+# Input:  data visualization, plot, barchart, time-series, panel analysis
+# ------------------------------------------------------  
+# Output:  
+# ------------------------------------------------------  
+# Example:  
+# ------------------------------------------------------  
 
+
+
+### CODE SECTION ###
+
+# Required Packages
 install.packages("ggplot2")
 install.packages("reshape2")
 install.packages("gridExtra")
@@ -11,31 +40,31 @@ library(ggplot2)
 library(reshape2)
 
 # Date is a data.frame but with Factors 
-UnempM <- read.csv("Arbeitslosenquote_Bundeslaender_13211-0008.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
-UnempJ <- read.csv("Arbeitslosenquote_Bundeslaender_year.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
-Google_Unemp  <- read.csv("Google_Arbeitsamt_Bundeslaender_monthly.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
-Google_Unemp_J <- read.csv("Google_Arbeitsamt_Bundeslaender_year.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
+UnempM = read.csv("Arbeitslosenquote_Bundeslaender_13211-0008.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
+UnempJ = read.csv("Arbeitslosenquote_Bundeslaender_year.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
+Google_Unemp  = read.csv("Google_Arbeitsamt_Bundeslaender_monthly.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
+Google_Unemp_J = read.csv("Google_Arbeitsamt_Bundeslaender_year.csv",header=TRUE,sep=";",dec=",",stringsAsFactors=TRUE)
 
 # some data frame transformations
-Google_Unemp_M <- melt(Google_Unemp_J,id.var="Date")
+Google_Unemp_M = melt(Google_Unemp_J,id.var="Date")
 str(Google_Unemp_M)
-Unemp_M <- melt(UnempJ,id.var="Date")
+Unemp_M = melt(UnempJ,id.var="Date")
 str(Google_Unemp_M)
 
-#### GG Plot - Säulendiagramm Gestapelt
+# GG Plot - stacked Barplot 
 # specify Colors
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 
-# Google "Arbeitsamt"
-Gplot_GoogleA <- ggplot(Google_Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
+# Barplot Google "Arbeitsamt"
+Gplot_GoogleA = ggplot(Google_Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
   geom_bar(stat="identity", position = "stack") +
   coord_flip() + scale_fill_manual(values = getPalette(16), guide_legend(title="Bundesländer")) +
   scale_y_continuous(expand = c(0,0)) + scale_x_reverse(breaks=2005:2015) +
   ggtitle("Google Suchanfragen nach 'Arbeitsamt'") + ylab("Frequenz") +
   theme(axis.text = element_text(size = 12))
 
-# Arbeitslosenquote Deutschland nach Bundesländern
-Gplot_Unemp <- ggplot(Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
+# Barplot Unemploymentrate 
+Gplot_Unemp = ggplot(Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
   geom_bar(stat="identity", position = "stack") +
   coord_flip() + scale_fill_manual(values = getPalette(16), guide_legend(title="Bundesländer")) +
   scale_y_continuous(trans = "reverse",expand = c(0,0)) + scale_x_reverse(breaks=2005:2015) +
@@ -45,20 +74,20 @@ Gplot_Unemp <- ggplot(Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.nume
   theme(axis.text.y = element_text(size = 12, color= "black")) + theme(axis.title.y = element_text(size=15)) +
   theme(plot.title = element_text(size = 20, vjust=2))
 
-# Gemeinsamme Legende
-#1.Schritt: Funktion zum speichern der Legende. Nur der Legende
-g_legend <- function(a.gplot){
-  tmp <- ggplot_gtable(ggplot_build(a.gplot))
-  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
-  legend <- tmp$grobs[[leg]]
+# Collective Legend
+# Step 1: Function to save only the legend
+g_legend = function(a.gplot){
+  tmp = ggplot_gtable(ggplot_build(a.gplot))
+  leg = which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend = tmp$grobs[[leg]]
   return(legend)
 }
 
-# 2. Schritt: Legende aus dem Plot extrahieren - Ergebnis ist nur die Legende
-leg<-g_legend(Gplot_GoogleA)
+# Step 2: Export only the legend from the first plot
+leg = g_legend(Gplot_GoogleA)
 
-# 3. Schritt: Legende aus Google Plot entfernen
-Gplot_GoogleA_oL <- ggplot(Google_Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
+# Step 3: Revome the legend from the plot
+Gplot_GoogleA_oL = ggplot(Google_Unemp_M,aes(x=Date, y=value, fill=variable, order=-as.numeric(value))) +
   geom_bar(stat="identity", position = "stack") + coord_flip() +
   scale_fill_manual(values = getPalette(16), guide_legend(title="Bundesländer")) +
   scale_y_continuous(expand = c(0,0)) + scale_x_reverse(breaks=2005:2015) +
@@ -66,7 +95,7 @@ Gplot_GoogleA_oL <- ggplot(Google_Unemp_M,aes(x=Date, y=value, fill=variable, or
   ylab("Frequenz") + theme(axis.text = element_text(size = 12, color= "black")) +
   theme(axis.title = element_text(size=15)) + theme(axis.text.y = element_text(size = 12, color= "black")) +
   theme(axis.title.y = element_text(size=15)) + theme(plot.title = element_text(size = 20, vjust=2))
-  #guides(fill=guide_legend(nrow=3,byrow=TRUE))
+  
 
-#4.Schritt: Graphen Plotten und Legende einfügen. 
+# Step 4: Plot the two Graphs and the legend
 grid.arrange(arrangeGrob(Gplot_Unemp,Gplot_GoogleA_oL,leg ,ncol=3,widths=c(3/7,3/7,1/7)))
